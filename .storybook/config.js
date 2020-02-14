5b9c4afd3d98b7cf4b9604d7f2b278b25114ac68
lmm1ng <50752476+lmm1ng@users.noreply.github.com>
2020-02-14 16:55:02 +0300

    git padges added

--- .storybook/config.js
+++
@@ -1,5 +0,0 @@
-import { configure } from '@storybook/react';
-import 'semantic-ui-css/semantic.min.css';
-
-// automatically import all files ending in *.story.jsx
-configure(require.context('../src', true, /\/(.*\.)?story\.jsx$/), module);
